0de7bd4a579676dd64a087457b8a88cccf3f4ab5
FilipSkoczylas <ffff3617@gmail.com>
2024-01-19 18:57:12 +0100

    Add localisation setters and getters

--- src/main/java/dev/przetrwaj/przetrwajapi/Localisation/Localisation.java
+++ src/main/java/dev/przetrwaj/przetrwajapi/Localisation/Localisation.java
@@ -14,4 +14,34 @@ public class Localisation
     private String adress;
     private Double longitude; //Długość geograficzna
     private Double latitude; //Szerokośc geograficzna
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
 }
